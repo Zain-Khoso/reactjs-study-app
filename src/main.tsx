8d1e38eb41c34469dcdd8fe86fb45583fefbebb7
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-10 12:48:11 +0500

    new: Tailwindcss Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// Lib Imports.
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root")!).render(
+// Assets.
+import './index.css';
+
+// Render Application.
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <h1>Hello World</h1>
   </StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// Node Imports.
+import path from 'path';
+
+// Lib Imports.
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
