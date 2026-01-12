da4ef79318ecb7747d28b6a98dd34e6441d196a5
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-12 21:32:40 +0500

    update: Navbar Responsiveness

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,6 @@
+// Lib Imports.
+import { Link } from 'react-router';
+
 // Component Imports.
 import BrandLogo from './ui/brand-logo';
 import { Button } from './ui/button';
@@ -7,6 +10,29 @@ export default function Navbar() {
   return (
     <nav className="border-b-muted flex w-full items-center justify-between border-b p-4">
       <BrandLogo />
+
+      <ul className="hidden items-center gap-4 md:flex">
+        <li>
+          <Link to="/arenas" className="text-muted-foreground">
+            Arena
+          </Link>
+        </li>
+        <li>
+          <Link to="/leaderboard" className="text-muted-foreground">
+            Leaderboard
+          </Link>
+        </li>
+        <li>
+          <Link to="/ranking" className="text-muted-foreground">
+            Ranking
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile" className="text-muted-foreground">
+            Profile
+          </Link>
+        </li>
+      </ul>
 
       <Button>Login</Button>
     </nav>
