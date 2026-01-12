7349ae74bd32f45c1d675d01e18c39f3f2c8ae92
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-12 14:53:24 +0500

    update: Live Pulse components

--- src/components/ui/live-pulse.tsx
+++ src/components/ui/live-pulse.tsx
@@ -10,11 +10,11 @@ type Props = {
 export default function LivePulse({ isLive = false }: Props) {
   return (
     <div className="flex items-center gap-1">
-      {isLive ? <Small className="text-primary">Live</Small> : <></>}
-
       <span
         className={cn('block h-2 w-2 rounded-full', isLive ? 'bg-primary' : 'bg-foreground')}
       ></span>
+
+      {isLive ? <Small className="text-primary font-semibold uppercase">Live</Small> : <></>}
     </div>
   );
 }
