354b3ffbd2c124a2e3a6b5471dc7d22e6e6af1e1
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-13 15:18:26 +0500

    fix: Brand logo link

--- src/components/ui/brand-logo.tsx
+++ src/components/ui/brand-logo.tsx
@@ -1,3 +1,6 @@
+// Lib Imports.
+import { Link } from 'react-router';
+
 // Assets.
 import { IconBoltFilled } from '@tabler/icons-react';
 
@@ -7,10 +10,10 @@ import { H3 } from './typography';
 // Component.
 export default function BrandLogo() {
   return (
-    <div className="flex items-center gap-2">
+    <Link to="/" className="flex items-center gap-2">
       <IconBoltFilled size={32} className="fill-primary" />
 
-      <H3 className="text-primary font-semibold">PrepPulse</H3>
-    </div>
+      <H3 className="text-primary font-semibold">Preplus</H3>
+    </Link>
   );
 }
