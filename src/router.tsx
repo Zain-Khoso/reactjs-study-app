fb2bbff54b195ee12f05acbe074af5e8d45581c9
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-13 22:08:53 +0500

    new: Terms and Conditions page

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ import GeneralLayout from './components/layouts/general';
 
 // Pages.
 import LandingPage from './pages/landing';
+import TermsAndConditionsPage from './pages/terms-and-conditions';
 
 // Application Routes.
 export default function Router() {
@@ -14,6 +15,7 @@ export default function Router() {
       <Routes>
         <Route element={<GeneralLayout />}>
           <Route index element={<LandingPage />} />
+          <Route path="terms-and-conditions" element={<TermsAndConditionsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
