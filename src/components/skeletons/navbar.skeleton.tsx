04a0d2d81d34e1beff35b5f13959740e6cfaf564
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-15 23:53:39 +0500

    fix: Navbar Skeleton

--- src/components/skeletons/navbar.skeleton.tsx
+++ src/components/skeletons/navbar.skeleton.tsx
@@ -4,20 +4,27 @@ import { Skeleton } from '@/components/ui/skeleton';
 // Loading Skeleton for Navbar.
 export default function NavbarSkeleton() {
   return (
-    <nav className="border-muted mx-auto mb-12 flex w-full max-w-7xl items-center justify-between border-b p-4">
-      <Skeleton className="h-10 w-32 rounded-md" />
+    <nav className="border-b-muted bg-background/80 sticky top-0 z-50 mx-auto mb-12 flex w-full max-w-7xl items-center justify-between border-b p-4 backdrop-blur-md">
+      <div className="flex items-center gap-8">
+        <Skeleton className="h-9 w-32 rounded-lg" />
 
-      <ul className="hidden items-center gap-6 md:flex">
-        {[1, 2, 3, 4].map((i) => (
-          <li key={i}>
-            <Skeleton className="h-4 w-20" />
-          </li>
-        ))}
-      </ul>
+        <ul className="hidden items-center gap-6 md:flex">
+          {[1, 2, 3, 4].map((i) => (
+            <li key={i}>
+              <Skeleton className="bg-foreground/5 h-4 w-20 rounded-md" />
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      <div className="flex items-center gap-2">
-        <Skeleton className="h-10 w-10 rounded-md" />
-        <Skeleton className="h-10 w-28 rounded-md" />
+      <div className="flex items-center gap-3">
+        <Skeleton className="bg-foreground/5 h-9 w-9 rounded-full" />
+
+        <div className="flex items-center gap-4">
+          <Skeleton className="bg-foreground/5 h-9 w-9 rounded-full" />
+
+          <Skeleton className="bg-foreground/10 h-9 w-9 rounded-full md:w-28 md:rounded-md" />
+        </div>
       </div>
     </nav>
   );
