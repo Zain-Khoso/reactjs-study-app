a01d6ffac18b0428f0a4a80f556f320ba2ef49fa
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-15 15:54:16 +0500

    new: SignInPage loading skeleton

--- src/components/layouts/empty.tsx
+++ src/components/layouts/empty.tsx
@@ -1,13 +1,28 @@
 // Lib Import.
 import { Suspense } from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import { AnimatePresence } from 'motion/react';
+
+// Page Skeletons.
+import SignInSkeleton from '@/pages/skeleton.signin';
 
 // Layout used for unique pages.
 export default function EmptyLayout() {
+  const { pathname } = useLocation();
+
+  const getSkeleton = () => {
+    switch (pathname) {
+      case '/signin':
+        return <SignInSkeleton key="signin-skeleton" />;
+
+      default:
+        return <SignInSkeleton key="signin-skeleton" />;
+    }
+  };
+
   return (
     <AnimatePresence>
-      <Suspense>
+      <Suspense fallback={getSkeleton()}>
         <Outlet />
       </Suspense>
     </AnimatePresence>
