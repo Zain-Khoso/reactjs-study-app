604c116554b10c884dbe1d9964f07913c8bab824
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-17 13:25:00 +0500

    new: Rank List component for Leaderboards page

---
+++ src/components/leaderboards/rank-list.tsx
@@ -0,0 +1,56 @@
+// Asset Imports.
+import { IconTrendingUp } from '@tabler/icons-react';
+
+// Component Imports.
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { P, Small } from '../ui/typography';
+
+// Types.
+import { type LeaderboardEntry } from './podium-item';
+type RankListProps = {
+  users: LeaderboardEntry[];
+};
+type RankListItemProps = {
+  user: LeaderboardEntry;
+};
+
+// Component used in leaderboards page to list users.
+export default function RankList({ users }: RankListProps) {
+  return (
+    <ul className="divide-border divide-y">
+      {users.map((user) => (
+        <RankListItem key={user.rank} user={user} />
+      ))}
+    </ul>
+  );
+}
+
+// Component used in leaderboards page to show a single user and its rank.
+function RankListItem({ user }: RankListItemProps) {
+  return (
+    <li className="hover:bg-accent/50 group flex items-center justify-between p-4 transition-all">
+      <div className="flex items-center gap-4 sm:gap-6">
+        <Small className="text-muted-foreground">{user.rank}</Small>
+
+        <div className="flex items-center gap-3">
+          <Avatar className="h-10 w-10">
+            <AvatarImage src={user.avatar} alt={user.name} />
+            <AvatarFallback>{user.name[0]}</AvatarFallback>
+          </Avatar>
+
+          <div>
+            <P className="leading-2">{user.name}</P>
+            <Small className="text-muted-foreground">{user.handle}</Small>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <P className="text-right font-bold">{user.points.toLocaleString()} pts</P>
+        <Small className="text-primary flex items-center justify-end gap-1 font-bold">
+          <IconTrendingUp size={12} stroke={3} />+{user.pointsToday} today
+        </Small>
+      </div>
+    </li>
+  );
+}
