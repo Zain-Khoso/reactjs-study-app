4f3994688cd7e5e98ba2837a756d3308336542ec
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-30 11:49:45 +0500

    update: navlink related logic extracted from navbar.

--- src/components/navigation/navlink-and-theme-toggle.tsx
+++ src/components/navigation/navlink-and-theme-toggle.tsx
@@ -1,7 +1,6 @@
 // Lib Imports.
-import { NavLink as BaseNavLink } from 'react-router';
+import { NavLink as BaseNavLink, useLocation } from 'react-router';
 import { HashLink } from 'react-router-hash-link';
-import { motion } from 'motion/react';
 
 // Asset Imports.
 import { IconSunFilled, IconMoonFilled } from '@tabler/icons-react';
@@ -12,16 +11,6 @@ import { useTheme } from '@/lib/hooks';
 
 // Component Imports.
 import { Button } from '../ui/button';
-
-// Types.
-type NavLinkProps = {
-  index: number;
-  label: string;
-  href: string;
-  isActive: boolean;
-  external: boolean;
-  hash?: boolean;
-};
 
 // Theme toggle icon button used in navbar.
 function ThemeToggle() {
@@ -45,19 +34,18 @@ function ThemeToggle() {
 }
 
 // Navigation Link.
-function NavLink({ index, href, label, isActive, external, hash = false }: NavLinkProps) {
+function NavLink({ href, label }: { label: string; href: string }) {
+  const { pathname } = useLocation();
+
+  const classNames = 'hover:text-primary text-sm font-medium transition-colors';
+  const external = href.startsWith('https://');
+  const isActive = !external && pathname.endsWith(href);
+  const hash = !external && href.includes('#');
+
   return (
-    <motion.li
-      initial={{ opacity: 0, y: -10 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.1 * index + 0.2 }}
-    >
+    <li>
       {hash ? (
-        <HashLink
-          to={href}
-          smooth
-          className="text-muted-foreground hover:text-primary text-sm font-medium transition-colors"
-        >
+        <HashLink to={href} smooth className={cn(classNames, 'text-muted-foreground')}>
           {label}
         </HashLink>
       ) : (
@@ -65,15 +53,15 @@ function NavLink({ index, href, label, isActive, external, hash = false }: NavLi
           to={href}
           target={external ? '_blank' : '_self'}
           className={cn(
-            'hover:text-primary text-sm font-medium transition-colors',
+            classNames,
             isActive ? 'text-primary underline underline-offset-4' : 'text-muted-foreground'
           )}
         >
           {label}
         </BaseNavLink>
       )}
-    </motion.li>
+    </li>
   );
 }
 
-export { type NavLinkProps, ThemeToggle, NavLink };
+export { ThemeToggle, NavLink };
