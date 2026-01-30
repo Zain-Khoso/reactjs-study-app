b9fbf365336d3f3880ae484d85d039ef21c2baed
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-30 21:04:16 +0500

    update: Logout state sync

--- src/components/profile/account-settings.tsx
+++ src/components/profile/account-settings.tsx
@@ -1,16 +1,28 @@
 // Lib Imports.
 import { motion } from 'motion/react';
+import { useDispatch } from 'react-redux';
 
 // Util Imports.
 import { itemVariants } from '@/lib/motions';
 import { signOut } from '@/lib/auth-client';
+import { resetCurrentUser } from '@/store/user.slice';
 
 // Component Imports.
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '../ui/button';
+import { useNavigate } from 'react-router';
 
 // Component used inside profile page to interact with account settings.
 export default function AccountSettings() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async function () {
+    await signOut();
+    navigate('/login');
+    dispatch(resetCurrentUser());
+  };
+
   return (
     <motion.div variants={itemVariants}>
       <Card>
@@ -18,7 +30,7 @@ export default function AccountSettings() {
           <CardTitle className="text-muted-foreground uppercase">Account Settings</CardTitle>
         </CardHeader>
         <CardContent className="space-y-4">
-          <Button variant="outline" className="w-full" onClick={() => signOut()}>
+          <Button variant="outline" className="w-full" onClick={handleLogout}>
             Logout
           </Button>
         </CardContent>
