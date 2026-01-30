138e5cebe592ce66bf4c48c379c28057ac2296a0
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-30 11:52:29 +0500

    fix: Separate static-nav component

---
+++ src/components/navigation/static-nav.tsx
@@ -0,0 +1,49 @@
+// Lib Imports.
+import { Link } from 'react-router';
+
+// Component Imports.
+import BrandLogo from '../ui/brand-logo';
+import { Button } from '../ui/button';
+import { NavLink, ThemeToggle } from './navlink-and-theme-toggle';
+
+// Data.
+const links = [
+  { label: 'Creator', href: 'https://linkedin.com/in/zain-khoso' },
+  {
+    label: 'Source Code',
+    href: 'https://github.com/zain-khoso/reactjs-study-app',
+  },
+  { label: 'Live Arenas', href: '/#arenas' },
+  { label: 'Assessment', href: '/#assessment' },
+];
+
+// Navbar for static pages.
+export default function Navbar() {
+  return (
+    <nav className="border-b-muted bg-background/80 sticky top-0 z-50 mx-auto mb-12 flex w-full max-w-7xl items-center justify-between border-b p-4 backdrop-blur-md">
+      <section className="flex items-center gap-8">
+        <BrandLogo />
+
+        <ul className="hidden items-center gap-6 md:flex">
+          {links.map(({ href, label }) => (
+            <NavLink key={`navbar-navlink-${label}-${href}`} href={href} label={label} />
+          ))}
+        </ul>
+      </section>
+
+      <section className="flex items-center gap-2">
+        <ThemeToggle />
+
+        <div className="space-x-2">
+          <Button asChild variant="link">
+            <Link to="/signin">Sign In</Link>
+          </Button>
+
+          <Button asChild>
+            <Link to="/arenas">Take a Quiz</Link>
+          </Button>
+        </div>
+      </section>
+    </nav>
+  );
+}
