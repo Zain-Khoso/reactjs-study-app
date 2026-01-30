a6d950adde40f7cbc296ae98c27f67f1ab68303b
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-30 19:40:40 +0500

    new: auth route loader

--- src/lib/data-loaders.tsx
+++ src/lib/data-loaders.tsx
@@ -1,8 +1,11 @@
+// Lib Imports.
+import { redirect } from 'react-router';
+
 // Util Imports.
 import store from '@/store';
 import { fetchCurrentUser } from '@/store/user.slice';
 
-// Data loader to get the current user and store it inside redux's user slice.
+// Route loader to get the current user and store it inside redux's user slice.
 async function fetchCurrentUserState() {
   const state = store.getState();
 
@@ -11,5 +14,16 @@ async function fetchCurrentUserState() {
   return null;
 }
 
+// Route loader that acts as a auth middleware.
+async function authenticationRequired(required: boolean) {
+  await fetchCurrentUserState();
+
+  const state = store.getState();
+
+  if (required && !state.user.data) return redirect('/login');
+
+  if (!required && state.user.data) return redirect('/arenas');
+}
+
 // Exports.
-export { fetchCurrentUserState };
+export { fetchCurrentUserState, authenticationRequired };
