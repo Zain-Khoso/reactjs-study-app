9b96c74cb556579981c14778d7367c94df31d4ff
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-01-31 12:14:25 +0500

    update: better-auth additional fields

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 // Lib Imports.
 import { createAuthClient } from 'better-auth/react';
+import { inferAdditionalFields } from 'better-auth/client/plugins';
 
 // Better-Auth client configuration.
 const authClient = createAuthClient({
@@ -7,12 +8,38 @@ const authClient = createAuthClient({
   fetchOptions: {
     credentials: 'include',
   },
+  plugins: [
+    inferAdditionalFields({
+      user: {
+        headline: {
+          type: 'string',
+          required: true,
+          defaultValue: 'Hi there! I am new.',
+        },
+        currentStreak: {
+          type: 'number',
+          required: true,
+          defaultValue: 0,
+        },
+        longestStreak: {
+          type: 'number',
+          required: true,
+          defaultValue: 0,
+        },
+      },
+    }),
+  ],
 });
 
 // Social Sign On helper.
 const signIn = (provider: string) =>
   authClient.signIn.social({ provider, callbackURL: import.meta.env.VITE_FRONTEND_URL });
 
+// Type Definitions.
+type Session = typeof authClient.$Infer.Session.session;
+type User = typeof authClient.$Infer.Session.user;
+
 // Exports.
 export { signIn };
+export type { Session, User };
 export const { signOut, getSession } = authClient;
